e821bf48c4d4a6d00f2116c34fb6077585b3d35d
FireEagle25 <nachaev.p.v.25@gmail.com>
2016-10-20 22:54:01 +1000

    Перенес классы лексем в отдельный класс

--- LexAnalizer.py
+++ LexAnalizer.py
@@ -1,44 +1,11 @@
 import re
+import LexType
 
 
 class Lexer:
-    lex_types = ["words", "bool_ops", "bools", "alg_ops", "comp_op", "brackets", "spec_symbols", "num", "float_num",
-                 "string", "var_name", "space", "string_start", "float_start"]
-
-    lexs_regex = {
-
-        0: [re.compile('^if$'), re.compile('^while$'), re.compile('^else$'),
-            re.compile('^elif$')],
-
-        1: [re.compile('^and$'), re.compile('^or$'), re.compile('^not$')],
-
-        2: [re.compile('^True$'), re.compile('^False')],
-
-        3: [re.compile('^\+$'), re.compile('^-$'), re.compile('^\*$'), re.compile('^\+=$'), re.compile('^-=$'),
-            re.compile('^\*=$'), re.compile('^=$')],
-
-        4: [re.compile('^<$'), re.compile('^>$'), re.compile('^<=$'), re.compile('^>=$'), re.compile('^==$')],
-
-        5: [re.compile('^\($'), re.compile('^\)$')],
-
-        6: [re.compile('^:$'), re.compile('^\t$')],
-
-        7: [re.compile('^[0-9]+$')],
-        8: [re.compile('^\d+\.\d+$')],
-
-        9: [re.compile('^".*"$')],
-
-        10: [re.compile('^[a-zA-Z_]+[a-zA-Z0-9]*$')],
-
-        11: [re.compile('^\s?$')],
-
-        12: [re.compile('^".*')],
-
-        13: [re.compile('^\d+\.$')]
-    }
 
     def __init__(self, filename):
-        self.isWork = True
+        self.work = True
         self.file = open(filename, 'r')
         self.ch = self.file.read(1)
 
@@ -53,19 +20,19 @@ class Lexer:
     def print_lex(self):
         if self.lex_type == 12:
             print("Ошибка, там строка не правильно написана, исправь, неуч!")
-            self.isWork = False
+            self.work = False
         elif self.lex_type == 13:
             print("Ошибка, там, это, флоат кривой")
-            self.isWork = False
+            self.work = False
         elif self.lex_type == 11:
             pass
         elif self.lex_type == 9:
-            print(self.lex[1:len(self.lex) -1] + "|" + Lexer.lex_types[self.lex_type] + "|line:" + str(self.line) + "|lex_pos:" + str(self.char_num_in_line))
+            print(self.lex[1:len(self.lex) -1] + "|" + self.lex_type + "|line:" + str(self.line) + "|lex_pos:" + str(self.char_num_in_line))
         elif self.lex_type is not None:
-            print(self.lex + "|" + Lexer.lex_types[self.lex_type] + "|line:" + str(self.line) + "|lex_pos:" + str(self.char_num_in_line))
+            print(self.lex + "|" + self.lex_type + "|line:" + str(self.line) + "|lex_pos:" + str(self.char_num_in_line))
         else:
             print("Ошибка, недопустимый символ")
-            self.isWork = False
+            self.work = False
 
         self.char_num_in_line += len(self.lex)
         self.lex = ""
@@ -85,7 +52,7 @@ class Lexer:
 
         while self.ch:
 
-            if not self.isWork:
+            if not self.work:
                 break
 
             if self.check_new_line():
@@ -95,19 +62,17 @@ class Lexer:
             if self.lex is not None:
                 lex_candidate = self.lex + self.ch
 
-            continue_old_lex = False
+            new_lex_type = LexType.get_type(lex_candidate)
 
-            for i in range(len(Lexer.lexs_regex)):
-                for lex_type in Lexer.lexs_regex[i]:
-                    if re.match(lex_type, lex_candidate) and not continue_old_lex:
-                        self.lex = lex_candidate
-                        self.lex_type = i
-                        continue_old_lex = True
-
-            if not continue_old_lex:
+            if not new_lex_type:
                 self.print_lex()
+                self.char_num_in_line += len(self.lex)
+                self.lex = ""
+                self.lex_type = None
             else:
                 self.next_ch()
+                self.lex = lex_candidate
+                self.lex_type = new_lex_type
 
 lexer = Lexer("1.txt")
 lexer.lex_analize()
---
+++ LexType.py
@@ -0,0 +1,42 @@
+import re
+
+lex_types = (
+
+        ("words", ['^if$', '^while$', '^else$',
+            '^elif$']),
+
+        ("bool_op", ['^and$', '^or$', '^not$']),
+
+        ("bools", ['^True$', '^False']),
+
+        ("alg_ops", ['^\+$', '^-$', '^\*$', '^\+=$', '^-=$',
+            '^\*=$', '^=$']),
+
+        ("comp_op", ['^<$', '^>$', '^<=$', '^>=$', '^==$']),
+
+        ("brackets", ['^\($', '^\)$']),
+
+        ("spec_symbols", ['^:$', '^\t$']),
+
+        ("num", ['^[0-9]+$']),
+        ("float_num", ['^\d+\.\d+$']),
+
+        ("string", ['^".*"$']),
+
+        ("var_name", ['^[a-zA-Z_]+[a-zA-Z0-9]*$']),
+
+        ("space", ['^\s?$']),
+
+        ("string_start", ['^".*']),
+
+        ("space", ['^\d+\.$'])
+)
+
+
+def get_type(string):
+
+        for type in lex_types:
+                for pattern in type[1]:
+                        if re.match(re.compile(pattern), string):
+                                return type[0]
+        return None
