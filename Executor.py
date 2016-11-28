57368c01f076110ca93de8f8305a0898d4348144
FireEagle25 <nachaev.p.v.25@gmail.com>
2016-11-28 22:35:08 +1000

    Синт. анализатор: сделал возврат дерева для метода parse

--- Executor.py
+++ Executor.py
@@ -5,8 +5,8 @@ from Parser.Parser import Parser
 def main(argv=None):
     file_name = "1.txt"
     lexs = Lexer(file_name).lex_analize()
-    parser = Parser(lexs)
-    parser.parse()
+    tree = Parser(lexs).parse()
+    print(tree)
 
 if __name__ == "__main__":
     main()
